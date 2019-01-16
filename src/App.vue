cc8cabcd67559ffda18a644b6120e34d559e95bc
Pasindu dineth <pasindu@vizuamatix.com>
2019-01-16 18:54:04 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template> 
+   <login/>
+</template>
+
+<script>
+import Login from './components/Login';
+export default {
+  name: 'app',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  
+}
+.app {
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: blueviolet;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+     <div class="app">
+<template>
+  <div>
+    <b-form style="backgroundColor:#fff;borderRadius:3%" class="p-4">
+        <h2 class="pb-2" style="color:#272727">vxHotel Management</h2>
+     <b-form-group id="exampleInputGroup1"
+                    label-for="exampleInput1">
+        <b-form-input id="exampleInput1"
+                      type="text"
+                      required
+                      placeholder="Email">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="exampleInputGroup2"
+                    label-for="exampleInput2">
+        <b-form-input id="exampleInput2"
+                      type="password"
+                      required
+                      placeholder="Password">
+        </b-form-input>
+      </b-form-group>
+      <b-button block type="submit" style="backgroundColor:blueviolet" class="block">Submit</b-button>
+    </b-form>
+  </div>
+</template>
+    </div>
+</template>
